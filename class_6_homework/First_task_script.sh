ba2267397e412743bb95885c1eb185ba61acc6ab
shafkat2 <shafkat.waheed@northsouth.edu>
2021-01-01 01:29:51 +0600

    updated the scripts

--- class_6_homework/First_task_script.sh
+++ class_6_homework/First_task_script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # propmts command for the bucket name
-read -p 'Bucket_Name: ' bucket
+bucket=$1
+file_name=$2
 
 # parse the json file to get if the bucket it enabled or not
-status=$(aws s3api get-bucket-versioning --bucket $bucket | awk '{print $3}' | grep -o '[A-Z].*abled')
-
+status=$(aws s3api get-bucket-versioning --bucket $bucket | awk '{print $2}' | grep -o '[A-Z].*.[a-z]')
+echo $status
 # checks the status and takes necessary action
-if [ "$status" == 'Disabled' ]; 
+if [ "$status" == 'Suspended' ]; 
 then
 
   aws s3api put-bucket-versioning --bucket $bucket --versioning-configuration Status=Enabled
@@ -20,8 +21,8 @@ then
 else
   echo "error due to wrong api call or bad request"
 fi
+aws s3api delete-public-access-block --bucket $bucket
 
+file_name_version_id=$(aws s3api delete-objects --bucket izaan-it-version-check --delete file://delete.json | awk '/DeleteMarkerVersionId/ {print $2}' | grep -o "[a-z A-Z 0-9].*.[a-z A-Z 0-9]")
 
-file_name_version_id = $(aws s3api delete-objects --bucket $bucket --delete ./delete.json | awk '/DeleteMarkerVersionId/ {print $2}' test.txt |  grep -o "[a-z].*[a-z]")
-
-aws s3api get-object --bucket $bucket --version-id $file_name_version_id
+aws s3api get-object --bucket $bucket --version-id $file_name_version_id $file_name
