dbe63f3e6044484feb95ce055a06788c05f65724
shafkat2 <shafkat.waheed@northsouth.edu>
2020-12-31 21:33:06 +0600

    working bash script one click

--- class_6_homework/Second_task_script.sh
+++ class_6_homework/Second_task_script.sh
@@ -3,35 +3,30 @@
 source=$1
 bucket_dest=$2
 
-# checks if the bucket is there or not
-check_bucket=$(aws s3 ls | awk '/'$bucket_dest'/ {print $3}')
 
-echo $check_bucket
 
-# if not present creates the bucket
-if [[ $check_bucket != $bucket_dest ]];
-then
-   aws s3api create-bucket --bucket $bucket_dest --region us-east-1
-fi
+# create bucket
+aws s3api create-bucket --bucket $bucket_dest --region us-east-1
+
 
 # copy the contents from source to dest
-aws s3 sync ./$source s3://$check_bucket
+aws s3 sync ./$source s3://$bucket_dest
 
 # check for bucket policy
-status_bucket=$(aws s3api get-bucket-policy-status --bucket $check_bucket | awk '/IsPublic/ {print $3}'| grep -o "[a-z].*[a-z]")
+status_bucket=$(aws s3api get-bucket-policy-status --bucket $bucket_dest | awk '/IsPublic/ {print $3}'| grep -o "[a-z].*[a-z]")
 
 
 # if no bucket policy deletes the block access
 if [[ $status_bucket != 'true' ]];
 then
-   aws s3api delete-public-access-block --bucket $check_bucket
+   aws s3api delete-public-access-block --bucket $bucket_dest
 fi
 
 
 # attaches the policy to the bucket
-aws s3api put-bucket-policy --bucket $check_bucket --policy file://acl_policy.json
+aws s3api put-bucket-policy --bucket $bucket_dest --policy file://acl_policy.json
 
 # configures the bucket website
-aws s3api put-bucket-website --bucket $check_bucket --website-configuration file://website_conf.json
+aws s3api put-bucket-website --bucket $bucket_dest --website-configuration file://website_conf.json
 
 echo "please check URL"
