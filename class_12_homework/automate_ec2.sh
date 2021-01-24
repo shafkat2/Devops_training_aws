708ef3a582efdfa268cd6863a01b369304c8119c
shafkat2 <shafkat.waheed@northsouth.edu>
2021-01-24 11:06:22 +0600

    mentoring session updated

--- class_12_homework/automate_ec2.sh
+++ class_12_homework/automate_ec2.sh
@@ -3,7 +3,7 @@
 
 launch_template_id=$(aws ec2 create-launch-template --launch-template-name TemplateForEncryption --launch-template-data file://launch_template.json | grep -n "LaunchTemplateId" | awk '{print $3}' | grep -o "[a-z].*.[0-9]")
 
-instacne_id=$(aws ec2 run-instances --launch-template $launch_template_id | grep -n "InstanceId" | awk '{print $3}' | grep -o "[a-z].*.[0-9]"))
+instacne_id=$(aws ec2 run-instances --launch-template '{"LaunchTemplateId":$instacne_id}' | grep -n "InstanceId" | awk '{print $3}' | grep -o "[a-z].*.[0-9]"))
 
 
 status=$(aws ec2 describe-instance-status --instance-id $instacne_id | grep -n "code" | awk '{print $3}' | grep -o "[0-9].[0-9]")
@@ -16,7 +16,7 @@ while true; do
     sleep 10
 
 
-public_ip=$(aws ec2 describe-intances --instance-ids $instacne_id | grep -n "PublicIp" | awk '{print $3}' | grep -0 "[0-9].*.[0-9]" )
+public_ip=$(aws ec2 describe-instances --instance-ids $instacne_id | grep -n "PublicIp" | awk '{print $3}' | grep -0 "[0-9].*.[0-9]" )
 
 echo $public_ip
 
--- class_12_homework/automate_loadbalancer_ALP.sh
+++ class_12_homework/automate_loadbalancer_ALP.sh
@@ -2,4 +2,5 @@
 
 
 
-aws elb create-load-balancer --load-balancer-name my-load-balancer --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets subnet-15aaab61 --security-groups sg-a61988c3
+aws elb create-load-balancer --load-balancer-name test_load_balancer --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets ubnet-30f6a97d --security-groups sg-0c239e75ab72bb619
+aws elb register-instances-with-load-balancer --load-balancer-name testloadbalancer --instances file://instances_to_add.json
